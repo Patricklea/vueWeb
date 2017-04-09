f025664ab3bc1ccfe7f41bc024907a7e434fa63f
patrick <575720013@qq.com>
2017-04-09 12:38:33 +0800

    完成detail页面及其4个嵌套路由子页面的轮廓

--- src/main.js
+++ src/main.js
@@ -6,16 +6,55 @@ import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 // 注意路径。虽然与文件夹是同级，但要加上./
 import IndexPage from './pages/index'
+import DetailPage from './pages/detail'
+import DetailAnaPage from './pages/detail/analysis'
+import DetailCouPage from './pages/detail/count'
+import DetailForPage from './pages/detail/forecast'
+import DetailPubPage from './pages/detail/publish'
+
+
+
 Vue.use(VueRouter)
 Vue.use(VueResource)
+
+
 let router = new VueRouter({
 	mode: 'history',
 	routes: [
+        //一级路由，index页面，路径就是/
 		{
 			path: '/',
 			// 这里的component没有s！！！
 			component: IndexPage
-		}
+		},
+        //一级路由，detail页面
+        {
+            path: '/detail',
+            component: DetailPage,
+            // 重定向：访问/detail的时候，自动跳转到它的子页面
+            redirect: 'detail/analysis',
+            //嵌套的子路由（二级路由）
+            children: [
+                //下面都是二级路由的各个页面
+                {
+                    // 嵌套子路由的路径不需要加/
+                    path: 'analysis',
+                    component: DetailAnaPage
+                },
+                {
+                    path: 'count',
+                    component: DetailCouPage
+                },
+                {
+                    path: 'forecast',
+                    component: DetailForPage
+                },
+                {
+                    path: 'publish',
+                    component: DetailPubPage
+                }
+            ]
+        }
 	]
 })
 
